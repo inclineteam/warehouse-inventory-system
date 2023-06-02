c0eba778c261bf694f690c61c666bf749b11ec40
John Aeron Sencil <116534603+snclrn@users.noreply.github.com>
2023-06-02 09:28:18 +0800

    arf

--- components/Nav.js
+++ components/Nav.js
@@ -52,7 +52,8 @@ const Nav = ({ navigation, menu }) => {
           alignItems: "center",
           position: "absolute",
           display: showMenu ? "flex" : "none",
-          left: "55%",
+          right: 0,
+          marginEnd: -20,
           top: "100%",
           padding: 28,
           borderTopLeftRadius: 15,
@@ -62,44 +63,96 @@ const Nav = ({ navigation, menu }) => {
           borderWidth: 2,
         }}
       >
-        
         <Text
-        style={{ padding: 7, fontSize: 15, fontWeight: "bold" }}
-        onPress={() => {{ navigation.navigate("Home") }}}
-        >Home<Image></Image></Text>
+          style={{ padding: 7, fontSize: 15, fontWeight: "bold" }}
+          onPress={() => {
+            {
+              navigation.navigate("Home");
+            }
+          }}
+        >
+          Home<Image></Image>
+        </Text>
         <Text
-        style={{ padding: 7, fontSize: 15, fontWeight: "bold" }}
-        onPress={() => {{ navigation.navigate("Home") }}}
-        >Acc. Information<Image></Image></Text>
+          style={{ padding: 7, fontSize: 15, fontWeight: "bold" }}
+          onPress={() => {
+            {
+              navigation.navigate("Home");
+            }
+          }}
+        >
+          Acc. Information<Image></Image>
+        </Text>
         <Text
-        style={{ padding: 7, fontSize: 15, fontWeight: "bold" }}
-        onPress={() => {{ navigation.navigate("Home") }}}
-        >Inventory<Image></Image></Text>
+          style={{ padding: 7, fontSize: 15, fontWeight: "bold" }}
+          onPress={() => {
+            {
+              navigation.navigate("Home");
+            }
+          }}
+        >
+          Inventory<Image></Image>
+        </Text>
         <Text
-        style={{ padding: 7, fontSize: 15, fontWeight: "bold" }}
-        onPress={() => {{ navigation.navigate("Home") }}}
-        >Stocks<Image></Image></Text>
+          style={{ padding: 7, fontSize: 15, fontWeight: "bold" }}
+          onPress={() => {
+            {
+              navigation.navigate("Home");
+            }
+          }}
+        >
+          Stocks<Image></Image>
+        </Text>
         <Text
-        style={{ padding: 7, fontSize: 15, fontWeight: "bold" }}
-        onPress={() => {{ navigation.navigate("Items") }}}
-        >Items<Image></Image></Text>
+          style={{ padding: 7, fontSize: 15, fontWeight: "bold" }}
+          onPress={() => {
+            {
+              navigation.navigate("Items");
+            }
+          }}
+        >
+          Items<Image></Image>
+        </Text>
         <Text
-        style={{ padding: 7, fontSize: 15, fontWeight: "bold" }}
-        onPress={() => {{ navigation.navigate("Home") }}}
-        >Transactions<Image></Image></Text>
+          style={{ padding: 7, fontSize: 15, fontWeight: "bold" }}
+          onPress={() => {
+            {
+              navigation.navigate("Home");
+            }
+          }}
+        >
+          Transactions<Image></Image>
+        </Text>
         <Text
-        style={{ padding: 7, fontSize: 15, fontWeight: "bold" }}
-        onPress={() => {{ navigation.navigate("Home") }}}
-        >Returns<Image></Image></Text>
+          style={{ padding: 7, fontSize: 15, fontWeight: "bold" }}
+          onPress={() => {
+            {
+              navigation.navigate("Home");
+            }
+          }}
+        >
+          Returns<Image></Image>
+        </Text>
         <Text
-        style={{ padding: 7, fontSize: 15, fontWeight: "bold" }}
-        onPress={() => {{ navigation.navigate("Home") }}}
-        >About<Image></Image></Text>
+          style={{ padding: 7, fontSize: 15, fontWeight: "bold" }}
+          onPress={() => {
+            {
+              navigation.navigate("Home");
+            }
+          }}
+        >
+          About<Image></Image>
+        </Text>
         <Text
-        style={{ padding: 7, fontSize: 15, fontWeight: "bold" }}
-        onPress={() => {{ navigation.navigate("Home") }}}
-        >WIS Contacts<Image></Image></Text>
-        
+          style={{ padding: 7, fontSize: 15, fontWeight: "bold" }}
+          onPress={() => {
+            {
+              navigation.navigate("Home");
+            }
+          }}
+        >
+          WIS Contacts<Image></Image>
+        </Text>
       </View>
     </View>
   );
